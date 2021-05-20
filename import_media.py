b23fa8c3b86f273c983d16b1b09b2c2dbd244251
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-05-20 19:46:46 +0200

    Saving media object

--- import_media.py
+++ import_media.py
@@ -4,6 +4,9 @@ import datetime as dt
 from bs4 import BeautifulSoup as bs
 import re
 import calendar
+import config as conf
+import pickle as pk
+import os.path
 
 
 def read_files(path: str) -> Set[str]:
@@ -14,16 +17,24 @@ def read_files(path: str) -> Set[str]:
         return set([])
 
 
-def parse_dates(urls: Set[str]) -> Dict[dt, List[Tuple[str, str]]]:
+def insert_new_url(d, date: dt.datetime, media: str, url: str):
+    if date not in d.keys():
+        d[date] = {media: [url]}
+    else:
+        media_dict = d[date]
+        if media not in media_dict.keys():
+            media_dict[media] = [url]
+        else:
+            media_dict[media].append(url)
+
+
+def parse_dates(urls: Set[str]) -> Dict[dt.datetime, Dict[str, List[str]]]:
     out_dict = {}
     for url in urls:
         if "https://ewn.co.za/" in url:
             raw_dt = url.strip("https://ewn.co.za/")[:10].split('/')
             date = dt.datetime(int(raw_dt[0]), int(raw_dt[1]), int(raw_dt[2]))
-            if date not in out_dict.keys():
-                out_dict[date] = [("EWN", url)]
-            else:
-                out_dict[date].append(("EWN", url))
+            insert_new_url(out_dict, date, "EWN", url)
         elif "https://apnews.com/" in url:
             page = requests.get(url)
             soup = bs(page.text, 'html.parser')
@@ -33,10 +44,7 @@ def parse_dates(urls: Set[str]) -> Dict[dt, List[Tuple[str, str]]]:
                 day_num = gs.group(2)
                 year_num = gs.group(3)
                 date = dt.datetime(int(year_num), int(month_num), int(day_num))
-                if date not in out_dict.keys():
-                    out_dict[date] = [("Associated Press", url)]
-                else:
-                    out_dict[date].append(("Associated Press", url))
+                insert_new_url(out_dict, date, "Associated Press", url)
         elif "https://www.bbc.com/" in url:
             page = requests.get(url)
             soup = bs(page.text, 'html.parser')
@@ -46,31 +54,32 @@ def parse_dates(urls: Set[str]) -> Dict[dt, List[Tuple[str, str]]]:
                 month_num = gs.group(2)
                 year_num = gs.group(1)
                 date = dt.datetime(int(year_num), int(month_num), int(day_num))
-                if date not in out_dict.keys():
-                    out_dict[date] = [("BBC", url)]
-                else:
-                    out_dict[date].append(("BBC", url))
+                insert_new_url(out_dict, date, "BBC", url)
         elif "https://www.theguardian.com/" in url:
             gs = re.match(r"^https://www.theguardian.com/[\w\-\/]*/(\d+)/(\w+)/(\d+)/.*$", url)
             day_num = gs.group(3)
             month_num = list(calendar.month_name).index(str(gs.group(2)).title())
             year_num = gs.group(1)
             date = dt.datetime(int(year_num), int(month_num), int(day_num))
-            if date not in out_dict.keys():
-                out_dict[date] = [("The Guardian", url)]
-            else:
-                out_dict[date].append(("The Guardian", url))
+            insert_new_url(out_dict, date, "The Guardian", url)
     return out_dict
 
 
-def get_data() -> Dict[dt, List[Tuple[str, str]]]:
+def get_data() -> Dict[dt.datetime, Dict[str, List[str]]]:
     """
     Reads the URLs from "./resources/media_articles.txt" and organizes these by date and news outlet.
     :return a dictionary with keys being datetime and values being strings to lists of URLs.
     """
-    # TODO: Convert to Dict[dt, Dict[str,str]]
-    urls = read_files("./resources/media_articles.txt")
-    return parse_dates(urls)
+    mu_pickle = "./media_urls.pickle"
+    if os.path.isfile(mu_pickle):
+        with open(mu_pickle, "rb") as mu:
+            out_dict = pk.load(mu)
+    else:
+        urls = read_files(conf.MEDIA_URLS_PATH)
+        out_dict = parse_dates(urls)
+        with open(mu_pickle, "wb") as mu:
+            pk.dump(out_dict, mu)
+    return out_dict
 
 
 if __name__ == "__main__":
