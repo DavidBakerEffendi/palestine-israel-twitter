c290f6bb64028c2563ac492d81b2c58db04a7429
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-06-03 20:08:11 +0200

    Have media analysis and dump working

--- analyze_media.py
+++ analyze_media.py
@@ -1,14 +1,10 @@
-from typing import Tuple, List, Set
+from typing import Tuple, List, Set, Dict
 
 import numpy as np
 
 import pandas as pd
 import json
 import expert_ai_api
-
-REQUEST_CAP = 120
-REQUEST_WINDOW = 60.0
-SECONDS_PER_REQUEST = REQUEST_WINDOW / REQUEST_CAP
 
 
 def import_data():
@@ -30,17 +26,38 @@ def chunks(lst, n):
         yield lst[i:i + n]
 
 
-def analyze_text(text: str) -> Tuple[float, Set[str], Set[str], Set[str]]:
+def lists_to_avgs(d: Dict[str, List[float]]) -> Dict[str, float]:
+    out_dict = {}
+    for k in d.keys():
+        out_dict[k] = float(np.mean(d[k]))
+    return out_dict
+
+
+def analyze_text(text: str) -> Tuple[float, Dict[str, float], Dict[str, float], Dict[str, float]]:
     sent = []
-    phrases = set()
-    e_traits = set()
-    b_traits = set()
+    phrases = {}
+    e_traits = {}
+    b_traits = {}
+
     for chunk in chunks(expert_ai_api.clean_text(text), 25):
         payload = " ".join(chunk)
         sent.append(expert_ai_api.obtain_sentiment(payload))
-        phrases |= set([x.value for x in expert_ai_api.obtain_key_phrases(payload)])
-        e_traits |= set([x.label for x in expert_ai_api.obtain_traits(payload)])
-        b_traits |= set([x.label for x in expert_ai_api.obtain_traits(payload, taxonomy="behavioral-traits")])
+        for x in expert_ai_api.obtain_key_phrases(payload):
+            if x.value in phrases.keys():
+                phrases[x.value].append(x.score)
+            else:
+                phrases[x.value] = [x.score]
+        for x in expert_ai_api.obtain_traits(payload):
+            if x.label in phrases.keys():
+                e_traits[x.label].append(x.score)
+            else:
+                e_traits[x.label] = [x.score]
+        for x in expert_ai_api.obtain_traits(payload, taxonomy="behavioral-traits"):
+            if x.label in phrases.keys():
+                b_traits[x.label].append(x.score)
+            else:
+                b_traits[x.label] = [x.score]
+    phrases, e_traits, b_traits = lists_to_avgs(phrases), lists_to_avgs(e_traits), lists_to_avgs(b_traits)
     return float(np.mean(sent)), phrases, e_traits, b_traits
 
 
@@ -51,7 +68,42 @@ if __name__ == "__main__":
 
     results_by_date = {}
     grps_by_date = df.groupby(["date"])
-    # for date, group in grps_by_date:
-    #     for g in group.iterrows():
-    #         print(g)
 
+    for date, group in grps_by_date:
+        print("Processing data for {}".format(date))
+        data = {
+            'sentiment': [],
+            'key_phrases': {},
+            'emotional_traits': {},
+            'behavioral_traits': {}
+        }
+        for _, row in group.iterrows():
+            sent, phrases, e_traits, b_traits = analyze_text(row['text'])
+            data['sentiment'].append(sent)
+            for k, v in phrases.items():
+                if k not in data['key_phrases'].keys():
+                    data['key_phrases'][k] = [v]
+                else:
+                    data['key_phrases'][k].append(v)
+            for k, v in e_traits.items():
+                if k not in data['emotional_traits'].keys():
+                    data['emotional_traits'][k] = [v]
+                else:
+                    data['emotional_traits'][k].append(v)
+            for k, v in b_traits.items():
+                if k not in data['behavioral_traits'].keys():
+                    data['behavioral_traits'][k] = [v]
+                else:
+                    data['behavioral_traits'][k].append(v)
+
+        data['sentiment'] = np.mean(data['sentiment'])
+        data['key_phrases'] = lists_to_avgs(data['key_phrases'])
+        data['emotional_traits'] = lists_to_avgs(data['emotional_traits'])
+        data['behavioral_traits'] = lists_to_avgs(data['behavioral_traits'])
+        results_by_date[str(date)] = data
+
+    with open('analyzed_media.json', 'w+') as f:
+        for date, results in results_by_date.items():
+            d = results.copy()
+            d['date'] = str(date)
+            f.write(json.dumps(d) + "\n")
